12fda0a02adf4cf2cb3595955ba5d399811caade
LunaH9708 <huanglu9708@163.com>
2026-09-21 15:47:45 +0800

    Delete worker.js

--- worker.js
+++
@@ -1,7 +0,0 @@
-// Cloudflare Workers 入口：把 public/ 里的静态文件（index.html）直接返回给访客。
-// 整个网站就是一个 index.html，零外部依赖、可离线打开。
-export default {
-  async fetch(request, env) {
-    return env.ASSETS.fetch(request);
-  }
-};
